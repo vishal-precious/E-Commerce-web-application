a7fd40f2bc8b1901c8e30be8989895dd43dd472a
vishal-precious <vishal.dikhit@preciousinfosystem.com>
2023-11-20 19:24:37 +0530

    Stripe Integration

---
+++ app/controllers/carts_controller.rb
@@ -0,0 +1,22 @@
+class CartsController < ApplicationController
+  def index
+    @cart_items = current_user.carts.first.cart_items
+  end
+
+  def addproduct
+    if current_user.carts.present?
+      @cart_item = current_user.carts.cart_items.new(product_id: params[:id])
+    else
+      cart = current_user.carts.create
+      @cart_item = cart.cart_items.new(product_id: params[:id])
+    end
+    @cart_item.save 
+    redirect_to carts_path
+  end
+
+  def removeproduct
+    @cart_item = CartItem.find_by(id: params[:id])
+    @cart_item.destroy
+    redirect_to carts_path
+  end
+end
